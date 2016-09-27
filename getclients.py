fe18596f3a87b1d21020786281a84b76638f356a
Laurenz <github@laure.nz>
2016-09-28 00:40:06 +0200

    Get user/pass and ip from argparse

--- getclients.py
+++ getclients.py
@@ -3,28 +3,30 @@
 import requests
 import re
 import sys
+import base64
+import argparse
 
-ROUTER_IP = '192.168.1.1'
-COOKIE = dict(Authorization='Basic XXXXXXXXXXXXXXXXXXXXXXXX') # put your auth cookie here
 POST_DATA = '''[LAN_HOST_ENTRY#0,0,0,0,0,0#0,0,0,0,0,0]0,4\r
 leaseTimeRemaining\r
 MACAddress\r
 hostName\r
 IPAddress\r
 '''
-HEADERS = {
-    'Content-Type': 'text/plain',
-    'Referer': 'http://' + ROUTER_IP + '/'
-}
-
 regex = r"\[.*\].\n.*\n.*=(.*)\n.*=(.*)\n.*=(.*)"
 subst = "\\3\\t\\1\\t\\2"
 
 
-def main():
+def main(ip, user, pw):
+    encoded_auth = bytes.decode(base64.b64encode(str.encode(user)+b":"+str.encode(pw)))
+    cookie = dict(Authorization='Basic '+encoded_auth)
+
+    headers = {
+        'Content-Type': 'text/plain',
+        'Referer': 'http://' + ip + '/'
+    }
 
     # Ask the router for a list of DHCP clients
-    r = requests.post('http://' + ROUTER_IP + '/cgi?5', data=POST_DATA, cookies=COOKIE, headers=HEADERS)
+    r = requests.post('http://' + ip + '/cgi?5', data=POST_DATA, cookies=cookie, headers=headers)
 
     if not r.ok:
         print("Got status " + str(r.status_code), file=sys.stderr)
@@ -41,4 +43,13 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Query a TP-Link router\'s DHCP leases')
+    parser.add_argument('-u', '--user', dest='user', type=str, default='admin',
+                        help='Username (default: admin)')
+    parser.add_argument('-p', '--password', dest='pw', type=str, default='admin',
+                        help='Password (default: admin)')
+    parser.add_argument('-i', '--ip', dest='ip', type=str, default='192.168.1.1',
+                        help='Router IP (default: 192.168.1.1)')
+
+    args = parser.parse_args()
+    main(args.ip, args.user, args.pw)
